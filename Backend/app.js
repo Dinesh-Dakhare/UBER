6118cdc3511422d5b75dca6c94f57cf92bfb6663
Dinesh Dakhare <dineshdakhare22@gmail.com>
2025-02-09 12:35:26 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export { app };
